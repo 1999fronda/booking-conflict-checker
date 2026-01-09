40986333838f7b2728427d2f38e77fb723f90f94
1999fronda <episcope14@gmail.com>
2026-01-09 23:11:14 +0800

    Added Pinia to the frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,4 @@
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
@@ -8,6 +9,8 @@ axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
+const pinia = createPinia();
 const app = createApp(App);
 app.use(router);
+app.use(pinia);
 app.mount("#app");
